d448ca25d0e900cd38ac4d706309bb2b1df46ad8
taejeong <sontaewjd@nate.com>
2024-05-05 20:41:09 +0900

    Vector ==연산자 tolerance 값 FLT_EPSILON=>10.0f * FLT_EPSILON으로 변경

--- PhysicsEngine/Vector2D.cpp
+++ PhysicsEngine/Vector2D.cpp
@@ -38,7 +38,7 @@ Vector2D Vector2D::operator-(const Vector2D& obj) const { return { _x - obj._x, 
 Vector2D& Vector2D::operator+=(const Vector2D& obj) { _x += obj._x; _y += obj._y; return *this; }
 bool Vector2D::operator==(const Vector2D& obj) const
 {
-	if (std::abs(_x - obj._x) < FLT_EPSILON && std::abs(_y - obj._y) < FLT_EPSILON) return true;
+	if (std::abs(_x - obj._x) < 10.0f * FLT_EPSILON && std::abs(_y - obj._y) < 10.0f * FLT_EPSILON) return true;
 	return false;
 }
 #pragma endregion
