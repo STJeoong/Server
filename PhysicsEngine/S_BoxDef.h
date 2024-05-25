72e21f938c58010c8308a170c559f6736a244038
taejeong <sontaewjd@nate.com>
2024-05-25 22:54:32 +0900

    bouncinessThreshold 초기값 5.0f으로 수정. 최솟값도 5.0f임.

--- PhysicsEngine/S_BoxDef.h
+++ PhysicsEngine/S_BoxDef.h
@@ -8,7 +8,7 @@ struct S_BoxDef
 	float density = 1.0f;
 	float friction = 0.4f;
 	float bounciness = 0.0f;
-	float bouncinessThreshold = 1.0f;
+	float bouncinessThreshold = 5.0f;
 	bool isTrigger = false;
 	Vector2D halfSize; // 회전이 적용되지 않은 값
 };
--- PhysicsEngine/S_CircleDef.h
+++ PhysicsEngine/S_CircleDef.h
@@ -7,7 +7,7 @@ struct S_CircleDef
 	float density = 1.0f;
 	float friction = 0.4f;
 	float bounciness = 0.0f;
-	float bouncinessThreshold = 1.0f;
+	float bouncinessThreshold = 5.0f;
 	bool isTrigger = false;
 	float radius = 1.0f;
 };
